3081b40985bbffea7b26e4dbfab80002a23433be
sumitrjagtap143 <sumitrjagtap143@outlook.com>
2020-07-17 16:26:11 +0000

    COMMIT POrt NUMBER

--- DockerWebApplication/Models/Repository.cs
+++ DockerWebApplication/Models/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,15 +19,35 @@ namespace DockerWebApplication.Models
         {
             _ihttpclient = httpClient;
         }
+
+        static string GetIPAddress()
+        {
+            String address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                address = stream.ReadToEnd();
+            }
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+
+            return address;
+        }
         public async Task<List<Employee>> GetAllEmployee()
         {
             string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
             Console.WriteLine(hostName);
             // Get the IP  
-            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            string myIP = GetIPAddress();//Dns.GetHostByName(hostName).AddressList[0].ToString();
+
+
+
 
             List<Employee> model = null;
-            var task = _ihttpclient.GetAsync($"https://{myIP}:5001/api/Employe")
+            var task = _ihttpclient.GetAsync($"https://{myIP}:5000/api/Employe")
           .ContinueWith((taskwithresponse) =>
           {
               var response = taskwithresponse.Result;
@@ -49,7 +70,7 @@ namespace DockerWebApplication.Models
             string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
             var jsonInString = JsonConvert.SerializeObject(employee);
 
-            var result = await _ihttpclient.PostAsync($"https://{myIP}:5001/api/Employe", new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+            var result = await _ihttpclient.PostAsync($"https://{myIP}:5000/api/Employe", new StringContent(jsonInString, Encoding.UTF8, "application/json"));
             if (result != null)
             {
                 return 1;
